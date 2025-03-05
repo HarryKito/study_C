a9efa45b32bcb09dd6cb3711ebf4e10ac00cdb98
haiiron <korhary@gmail.com>
2025-03-06 02:01:18 +0900

    types

--- basicIO/main.cpp
+++ basicIO/main.cpp
@@ -8,6 +8,14 @@
 
 int main() {
     puts("Hi, Cpp");
-    printf("CMake does work well !? ::%s::",test_include ? "yes":"nope");
+    printf("CMake does work well !? ::%s::\n",test_include ? "yes":"nope");
+
+    printf("%-12s : %2zu byte\n", "char", sizeof(char));
+    printf("%-12s : %2zu byte\n", "short", sizeof(short));
+    printf("%-12s : %2zu byte\n", "int", sizeof(int));
+    printf("%-12s : %2zu byte\n", "long", sizeof(long));
+    printf("%-12s : %2zu byte\n", "long long", sizeof(long long));
+    printf("%-12s : %2zu byte\n", "float", sizeof(float));
+    printf("%-12s : %2zu byte\n", "double", sizeof(double));
     return 0;
 }
